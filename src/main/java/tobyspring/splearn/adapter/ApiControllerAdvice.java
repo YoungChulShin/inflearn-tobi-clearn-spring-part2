f785a761db962f5d194ca4eca2a53cfaf09d4265
YoungChulShin <go1323@gmail.com>
2026-08-24 23:15:40 +0900

    코드 오류 수정

--- src/main/java/tobyspring/splearn/adapter/ApiControllerAdvice.java
+++ src/main/java/tobyspring/splearn/adapter/ApiControllerAdvice.java
@@ -18,7 +18,7 @@ public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({DuplicateEmailException.class, DuplicateProfileException.class})
-    public ProblemDetail emailExceptionHandler(DuplicateEmailException exception) {
+    public ProblemDetail duplicateException(RuntimeException exception) {
         return getProblemDetail(HttpStatus.CONFLICT, exception);
     }
 
